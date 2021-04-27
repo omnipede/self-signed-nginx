0a5dec1de563b7690b0e038533fd8c8aaba4af66
서현규 <omnipede@coinplug.com>
2021-04-27 16:49:20 +0900

    Feature: add user input prompt script

---
+++ script/prompt.sh
@@ -0,0 +1,11 @@
+# User input prompt
+while true; do
+  printf "[!] This action will not be rolled back. Continue? [y/N] "
+  read -r yn
+
+  case $yn in
+    [Yy]* ) break;;
+    [Nn]* ) echo "Exit."; exit;;
+    * ) echo "Please answer yes or no.";;
+  esac
+done
